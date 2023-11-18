f023e888cb746f779dab7eae728c770242b33edc
tamarShapira <116269580+tamarShapira@users.noreply.github.com>
2023-11-18 20:59:43 +0200

    Update service.js=add dotenv

--- src/service.js
+++ src/service.js
@@ -13,26 +13,26 @@ axios.interceptors.request.use(function(config){
 
 export default {
   getTasks: async () => {
-    const result = await axios.get(`${apiUrl}/items`);
-    console.log('url=',`${apiUrl}/items`)
+    const result = await axios.get(`${process.env.REACT_APP_API_URL}/items`);
+    console.log('url=',`${process.env.REACT_APP_API_URL}/items`)
     return result.data;
   },
 
   addTask: async(name)=>{
     console.log('addTask', name);
-    const result=await axios.post(`${apiUrl}/addTodo`,{name,isComplete:false});
+    const result=await axios.post(`${process.env.REACT_APP_API_URL}/addTodo`,{name,isComplete:false});
     return result.data;
   },
 
   setCompleted:async(id,isComplete)=>{
     console.log('setCompleted', {id, isComplete})
-    const result=await axios.put(`${apiUrl}/addTodo/${id}?isComplete=${isComplete}`,id,isComplete);
+    const result=await axios.put(`${process.env.REACT_APP_API_URL}/addTodo/${id}?isComplete=${isComplete}`,id,isComplete);
     return result;
   },
 
   deleteTask:async(id)=>{
     console.log('deleteTask')
-    const result=await axios.delete(`${apiUrl}/deleteTodo/${id}`,id);
+    const result=await axios.delete(`${process.env.REACT_APP_API_URL}/deleteTodo/${id}`,id);
     return result.data;
   }
 };
